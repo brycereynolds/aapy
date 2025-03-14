b51066f60e8fd3dbd332458ccd298ba12908e231
Bryce Reynolds <brycereynoldsdesign@gmail.com>
2025-03-14 10:54:35 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Replace with your actual account ID
-ACCOUNT_ID=""
-
 # Create the output directory if it doesn't exist
 mkdir -p books
 
 # Run the script in interactive mode
-python aapy.py interactive --account-id "$ACCOUNT_ID" --output books/
+python aapy.py interactive
 
